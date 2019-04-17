8c3816af6370ef19a34a9dc0bb14ec36a96074eb
Abhya <abhya.tripathi.at@gmail.com>
2019-04-17 21:58:04 +0530

    search and post tweet added

--- functions.php
+++ functions.php
@@ -13,6 +13,86 @@
             session_unset();
 
         }
+        function time_since($since) {
+        $chunks = array(
+            array(60 * 60 * 24 * 365 , 'year'),
+            array(60 * 60 * 24 * 30 , 'month'),
+            array(60 * 60 * 24 * 7, 'week'),
+            array(60 * 60 * 24 , 'day'),
+            array(60 * 60 , 'hour'),
+            array(60 , 'min'),
+            array(1 , 'sec')
+        );
 
+        for ($i = 0, $j = count($chunks); $i < $j; $i++) {
+            $seconds = $chunks[$i][0];
+            $name = $chunks[$i][1];
+            if (($count = floor($since / $seconds)) != 0) {
+                break;
+            }
+        }
+
+        $print = ($count == 1) ? '1 '.$name : "$count {$name}s";
+        return $print;
+    }
+
+        function displayTweets($type){
+
+          global $link;
+
+          if($type == 'public'){
+            $whereclause = "";
+          }
+          $query = "SELECT * FROM tweets ".$whereclause." ORDER BY `date` DESC LIMIT 10";
+
+          $result = mysqli_query($link, $query);
+          if (mysqli_num_rows($result)==0){
+            echo "There are no tweets to display.";
+          }
+          else{
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                  $userQuery = "SELECT * FROM users WHERE id = ".mysqli_real_escape_string($link, $row['id'])." LIMIT 1";
+                  $userQueryResult = mysqli_query($link, $userQuery);
+                  $user = mysqli_fetch_assoc($userQueryResult);
+
+                  echo "<div class='tweet'><p><a href='?page=publicprofiles&userid=".$user['id']."'>".$user['email']."</a> <span class='time'>".time_since(time() - strtotime($row['date']))." ago</span>:</p>";
+
+                  echo "<p>".$row['tweet']."</p>";
+                  echo "<p>follow</p></div>";
+          }
+        }
+      }
+      function displaySearch() {
+
+      echo '<form class="form-inline">
+<div class="form-group">
+  <input type="hidden" name="page" value="search">
+  <input type="text" name="q" class="form-control" id="search" placeholder="Search">
+</div>
+<button type="submit" class="btn btn-primary">Search Tweets</button>
+</form>';
+
+
+  }
+
+  function displayTweetBox() {
+
+      if (isset($_SESSION['id']) > 0) {
+
+          echo '<div id="tweetSuccess" class="alert alert-success">Your tweet was posted successfully.</div>
+          <div id="tweetFail" class="alert alert-danger"></div>
+          <div class="form">
+<div class="form-group">
+  <textarea class="form-control" id="tweetContent"></textarea>
+</div>
+<button id="postTweetButton" class="btn btn-primary">Post Tweet</button>
+</div>';
+
+
+      }
+
+
+  }
 
  ?>
--- views/home.php
+++ views/home.php
@@ -1,3 +1,15 @@
-<div class="container">
-<h1>Hello, world!</h1>
+<div class="container mainContainer">
+  <div class="row">
+    <div class="col-sm-8">
+
+        <h2>Recent Tweets</h2>
+
+        <?php displayTweets('public'); ?>
+    </div>
+    <div class="col-sm-4">col-sm-4</div>
+
+    <?php displaySearch(); ?>
+    <hr>
+    <?php displayTweetBox(); ?>
+  </div>
 </div>
